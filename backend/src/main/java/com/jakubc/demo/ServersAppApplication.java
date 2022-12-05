b8b9469754bccaa77d9891a912a3918ed13a8930
Jakub <jakub.bchyla@gmail.com>
2022-12-05 18:27:28 +0100

    first commit

---
+++ backend/src/main/java/com/jakubc/demo/ServersAppApplication.java
@@ -0,0 +1,13 @@
+package com.jakubc.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServersAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServersAppApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/jakubc/demo/ServersAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jakubc.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServersAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
