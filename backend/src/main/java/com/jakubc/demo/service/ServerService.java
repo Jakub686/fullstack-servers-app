020310db481c8b409f9d4e55945cd97eef7388ac
Jakub <jakub.bchyla@gmail.com>
2022-12-06 06:16:17 +0100

    34:15

--- backend/src/main/java/com/jakubc/demo/service/ServerService.java
+++ backend/src/main/java/com/jakubc/demo/service/ServerService.java
@@ -3,12 +3,13 @@ package com.jakubc.demo.service;
 import com.jakubc.demo.model.Server;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.util.Collection;
 
 @Service
 public interface ServerService {
     Server create(Server server);
-    Server ping(String ipAddress);
+    Server ping(String ipAddress) throws IOException;
     Collection<Server> list (int limit);
     Server get(Long id);
     Server update(Server server);
--- backend/src/main/java/com/jakubc/demo/service/implementatnion/ServerServiceImpl.java
+++ backend/src/main/java/com/jakubc/demo/service/implementatnion/ServerServiceImpl.java
@@ -1,38 +1,80 @@
 package com.jakubc.demo.service.implementatnion;
 
 import com.jakubc.demo.model.Server;
+import com.jakubc.demo.repo.ServerRepo;
 import com.jakubc.demo.service.ServerService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.transaction.Transactional;
+import java.io.IOException;
+import java.net.InetAddress;
 import java.util.Collection;
+import java.util.Random;
 
+import static com.jakubc.demo.enumeration.Status.SERVER_DOWN;
+import static com.jakubc.demo.enumeration.Status.SERVER_UP;
+import static java.lang.Boolean.TRUE;
+
+@RequiredArgsConstructor
+@Service
+@Transactional
+@Slf4j
 public class ServerServiceImpl implements ServerService {
+
+    private final ServerRepo serverRepo;
+
     @Override
     public Server create(Server server) {
-        return null;
+        log.info("Saving new server: {}", server.getName());
+        server.setImageUrl(setServerImageUrl());
+        return serverRepo.save(server);
     }
 
     @Override
-    public Server ping(String ipAddress) {
-        return null;
+    public Server ping(String ipAddress) throws IOException {
+        log.info("Pinging server IP: {}", ipAddress);
+        Server server = serverRepo.findByIpAddress(ipAddress);
+        InetAddress address = InetAddress.getByName(ipAddress);
+        server.setStatus(address.isReachable(10000) ? SERVER_UP : SERVER_DOWN);
+        serverRepo.save(server);
+        return server;
     }
 
     @Override
     public Collection<Server> list(int limit) {
-        return null;
+        log.info("Fetching all servers");
+        return serverRepo.findAll(PageRequest.of(0,limit)).toList();
     }
 
     @Override
     public Server get(Long id) {
-        return null;
+        log.info("Fetching server by id: {}", id);
+        return serverRepo.findById(id).get();
     }
 
     @Override
     public Server update(Server server) {
-        return null;
+        log.info("Updating server: {}", server.getName());
+        return serverRepo.save(server);
     }
 
     @Override
     public Boolean delete(Long id) {
-        return null;
+        log.info("Deleting server by ID: {}", id);
+        serverRepo.deleteById(id);
+        return TRUE;
+    }
+
+    private String setServerImageUrl() {
+        String[] imageNames = {"server1.png","server2.png","server3.png","server4.png"};
+
+        return ServletUriComponentsBuilder
+                .fromCurrentContextPath()
+                .path("/server/image/" + imageNames[new Random().nextInt(4)])
+                        .toUriString();
     }
 }
