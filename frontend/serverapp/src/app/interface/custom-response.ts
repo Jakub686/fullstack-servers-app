39df66776e1c2d2fffafb298364d012a64f2455f
Jakub <jakub.bchyla@gmail.com>
2022-12-08 06:11:27 +0100

    1:24:26

---
+++ frontend/serverapp/src/app/interface/custom-response.ts
@@ -0,0 +1,11 @@
+import { Server } from "./server";
+
+export interface CustomRespone {
+    timeStamp: Date;
+    statusCode: number;
+    status: string;
+    reason: string;
+    message: string;
+    developerMessage: string;
+    data: { servers?: Server[], server?: Server };
+}
