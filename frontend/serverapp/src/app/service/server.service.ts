e56d75e9a84f3788f31e31fd8b97306bbe3fa9de
Jakub <jakub.bchyla@gmail.com>
2022-12-09 06:00:45 +0100

    1:44:00

--- frontend/serverapp/src/app/service/server.service.ts
+++ frontend/serverapp/src/app/service/server.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, Subscriber, throwError } from 'rxjs';
 import { tap, catchError } from 'rxjs/operators';
 import { CustomResponse } from '../interface/custom-response';
 import { Server } from '../interface/server';
+import { Status } from '../enum/status.enum';
 
 @Injectable({  providedIn: 'root'})
 export class ServerService {
@@ -33,6 +34,30 @@ export class ServerService {
     catchError(this.handleError)
   );
 
+  filter$ = (server: Status, respone: CustomResponse) => <Observable<CustomResponse>>
+  new Observable<CustomResponse>(
+    Subscriber => {
+      console.log(respone)
+      Subscriber.next(
+        status === Status.ALL ? {...respone, message: `Servers filtred by ${status} status` } :
+        {
+          ...respone,
+          message: respone.data.servers
+          .filter(server => server.status === status).length > 0 ? `Servers filtred by
+           ${status === Status.SERVER_UP ? `SERVER_UP` 
+           : `SERVER_DOWN` } status` : `No servers of ${status} found`,
+           data: { servers: respone.data.servers
+            .filter(server => server.status === status) }
+        }
+      );
+      Subscriber.complete();
+    }
+  )
+  .pipe(
+    tap(console.log),
+    catchError(this.handleError)
+  );
+
   delete$ = (serverId: number) => <Observable<CustomResponse>>
   this.http.delete<CustomResponse>(`${this.apiUrl}/server/delete/${serverId}`)
   .pipe(
@@ -40,8 +65,8 @@ export class ServerService {
     catchError(this.handleError)
   );
 
-
-  handleError(handleError: any): Observable<never> {
-    return throwError ('Method not implemented')
+  private handleError(error: HttpErrorResponse): Observable<never> {
+    console.log(error)
+    return throwError (`An error occurred - Error code: ${error.status}`);
   }
 }
